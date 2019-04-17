17d14ea87e58ccee1d307984421ab1041b8cd3be
pmrg.desarrollo <pmrg.desarrollo@gmail.com>
2019-04-16 23:24:18 -0500

    ahi va

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { PortadaComponent } from './components/portada/portada.component';
+import { TablaComponent } from './components/tabla/tabla.component';
+import { DescripcionesComponent } from './components/descripciones/descripciones.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    BannerComponent,
+    PortadaComponent,
+    TablaComponent,
+    DescripcionesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/descripciones/descripciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-descripciones',
+  templateUrl: './descripciones.component.html',
+  styles: []
+})
+export class DescripcionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
