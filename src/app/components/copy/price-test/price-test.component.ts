592199eea7571f5638e32ed9fe8d4dc70daf15d3
pmrg.desarrollo <pmrg.desarrollo@gmail.com>
2019-04-18 21:26:22 -0500

    avances

--- src/app/components/copy/price-test/price-test.component.ts
+++ src/app/components/copy/price-test/price-test.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-price-test',
-  templateUrl: './copy/price-test.component.html',
+  templateUrl: './price-test.component.html',
   styles: []
 })
 export class PriceTestComponent implements OnInit {
